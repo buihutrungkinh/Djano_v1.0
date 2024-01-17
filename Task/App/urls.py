b2e73de8425cb129bc13248a2cf4ce93408823b4
38002 <TamNQ3@fpt.com>
2024-01-17 11:43:47 +0700

    a

---
+++ Task/App/urls.py
@@ -0,0 +1,11 @@
+# myapp/urls.py
+
+from django.urls import path
+from .views import user_login, user_logout, user_register
+
+urlpatterns = [
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', user_register, name='register'),
+    # Các URL khác của ứng dụng
+]
